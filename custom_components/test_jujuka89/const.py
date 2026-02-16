f99da5bf7199087fb6c2d0310ddc71445876ebaa
Jujuka89 <74470508+Jujuka89@users.noreply.github.com>
2026-02-16 21:01:48 +0100

    Update const.py

--- custom_components/test_jujuka89/const.py
+++ custom_components/test_jujuka89/const.py
@@ -2,10 +2,8 @@
 
 from homeassistant.const import Platform
 
-DOMAIN = "test_jujuka89"
+DOMAIN = "dju"
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
-CONF_NAME = "name"
-CONF_DEVICE_ID = "device_id"
 DEFAULT_BASE_TEMP = 18.0
-DEVICE_MANUFACTURER = "JUJUKA89"
+
